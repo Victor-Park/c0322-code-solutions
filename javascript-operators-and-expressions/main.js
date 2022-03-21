3d7d0bb113c6a9246236742c36460cfc3b6ce4e4
Victor-Park <jsxpark@yahoo.com>
2022-03-21 19:35:18 +0000

    completed javascript-operators-and-expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,46 @@
+var width = 5;
+var height = 3;
+var area = width * height;
+
+console.log('value of Area', area);
+console.log(typeof area);
+
+var bill = 100;
+var payment = 150;
+var change = payment - bill;
+
+console.log('value of Change', change);
+console.log(typeof change);
+
+var quizzes = 76;
+var midterm = 89;
+var final = 92;
+var grade = (quizzes + midterm + final) / 3;
+
+console.log('value of Grade', grade);
+console.log(typeof grade);
+
+var firstName = 'Victor';
+var lastName = 'Park';
+var fullName = firstName + ' ' + lastName;
+
+console.log('Full Name', fullName);
+console.log(typeof fullName);
+
+var pH = 3;
+var isAcidic = pH < 7;
+
+console.log('is it Acidic?', isAcidic);
+console.log(typeof isAcidic);
+
+var headCount = 17;
+var isSparta = headCount === 300;
+
+console.log('value of isSparta', isSparta);
+console.log(typeof isSparta);
+
+var motto = fullName;
+motto = motto + ' is the GOAT';
+
+console.log('value of Motto', motto);
+console.log(typeof motto);
