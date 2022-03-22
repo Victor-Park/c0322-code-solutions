1681a3cc9be1b6061030fc0717d9f199939dc8ce
Victor-Park <jsxpark@yahoo.com>
2022-03-22 18:10:57 +0000

    completed javascript-data-model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,74 @@
+var orderHistory = [
+  {
+    orderDate: '8/4/2020',
+    orderTotal: '$34.00',
+    shipTo: 'JS Masher',
+    orderNumber: '#114-3941689-8772232',
+    deliveryDate: '8/8/2020',
+    itemName: 'JavaScript for impatient programmers',
+    author: 'Rauschmayer, Dr. Axel',
+    itemCost: '$31.55',
+    returnClose: '9/7/2020',
+    deliveryMethod: null,
+    productReview: false,
+    productSupport: false
+  },
+  {
+    orderDate: '7/19/2020',
+    orderTotal: '$44.53',
+    shipTo: 'JS Masher',
+    orderNumber: '#113-9984268-1280257',
+    deliveryDate: '7/20/2020',
+    itemName: 'The Timeless Way of Building',
+    author: 'Alexandaer, Christopher',
+    itemCost: '$41.33',
+    returnClose: '8/19/2020',
+    deliveryMethod: 'Directly handed to resident',
+    productReview: false,
+    productSupport: false
+  },
+  {
+    orderDate: '7/4/2020',
+    orderTotal: '$17.22',
+    shipTo: 'JS Masher',
+    orderNumber: '#114-2875557-9059409',
+    deliveryDate: '7/7/2020',
+    itemName: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+    author: null,
+    itemCost: '$15.98',
+    returnClose: '8/5/2020',
+    deliveryMethod: 'Directly handed to resident',
+    productReview: false,
+    productSupport: false
+  },
+  {
+    orderDate: '7/3/2020',
+    orderTotal: '$138.93',
+    shipTo: 'JS Masher',
+    orderNumber: '#113-2883177-2648248',
+    deliveryDate: '7/5/2020',
+    itemName: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+    author: null,
+    itemCost: '$94.95',
+    returnClose: '8/4/2020',
+    deliveryMethod: null,
+    productReview: false,
+    productSupport: true
+  },
+  {
+    orderDate: '7/3/2020',
+    orderTotal: '$138.93',
+    shipTo: 'JS Masher',
+    orderNumber: '#113-2883177-2648248',
+    deliveryDate: '7/5/2020',
+    itemName: 'The Art of Sql',
+    author: 'Faroult, Stephanie',
+    itemCost: '$33.99',
+    returnClose: '8/4/2020',
+    deliveryMethod: null,
+    productReview: false,
+    productSupport: true
+  }
+];
+
+console.log('orderhistory', orderHistory);
