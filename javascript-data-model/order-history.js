ffff683067c68b9c132ea7fac48a2a2c0d2b93ff
Victor-Park <jsxpark@yahoo.com>
2022-03-22 21:40:54 +0000

    revised javascript-data-model

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -5,13 +5,16 @@ var orderHistory = [
     shipTo: 'JS Masher',
     orderNumber: '#114-3941689-8772232',
     deliveryDate: '8/8/2020',
-    itemName: 'JavaScript for impatient programmers',
-    author: 'Rauschmayer, Dr. Axel',
-    itemCost: '$31.55',
     returnClose: '9/7/2020',
     deliveryMethod: null,
     productReview: false,
-    productSupport: false
+    item: [
+      {
+        itemName: 'JavaScript for impatient programmers',
+        itemCost: '$31.55',
+        author: 'Rauschmayer, Dr. Axel'
+      }
+    ]
   },
   {
     orderDate: '7/19/2020',
@@ -19,13 +22,16 @@ var orderHistory = [
     shipTo: 'JS Masher',
     orderNumber: '#113-9984268-1280257',
     deliveryDate: '7/20/2020',
-    itemName: 'The Timeless Way of Building',
-    author: 'Alexandaer, Christopher',
-    itemCost: '$41.33',
     returnClose: '8/19/2020',
     deliveryMethod: 'Directly handed to resident',
     productReview: false,
-    productSupport: false
+    item: [
+      {
+        itemName: 'The Timeless Way of Building',
+        itemCost: '$41.33',
+        author: 'Alexandaer, Christopher'
+      }
+    ]
   },
   {
     orderDate: '7/4/2020',
@@ -33,13 +39,16 @@ var orderHistory = [
     shipTo: 'JS Masher',
     orderNumber: '#114-2875557-9059409',
     deliveryDate: '7/7/2020',
-    itemName: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
-    author: null,
-    itemCost: '$15.98',
     returnClose: '8/5/2020',
     deliveryMethod: 'Directly handed to resident',
     productReview: false,
-    productSupport: false
+    item: [
+      {
+        itemName: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+        itemCost: '$15.98',
+        author: null
+      }
+    ]
   },
   {
     orderDate: '7/3/2020',
