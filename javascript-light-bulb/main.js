2469653c229fe46e1593585dc13900308d8cf7a2
Victor-Park <jsxpark@yahoo.com>
2022-03-29 22:56:29 +0000

    completed javascript-light-bulb

---
+++ javascript-light-bulb/main.js
@@ -0,0 +1,19 @@
+// assign return of querySelector method of document object with 1 argument, 'bulb', to new variable bulb
+// create addEventListener method of bulb object that listens for 'click', and calls new functino click with button parameter
+// assign null to new variable onoff
+// if the button class is 'off'
+// turn it on
+// else (button is already on), turn it off
+var container = document.querySelector('.container');
+var bulb = document.querySelector('.bulb');
+bulb.addEventListener('click', function click(button) {
+  var onoff;
+  if (bulb.className === 'bulb off') {
+    onoff = 'on';
+  } else {
+    onoff = 'off';
+  }
+  bulb.className = 'bulb ' + onoff;
+  container.className = 'container ' + onoff;
+}
+);
