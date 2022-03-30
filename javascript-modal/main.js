c1004f9c5a1c7c6946f8d6711fa729a891ce3a9c
Victor-Park <jsxpark@yahoo.com>
2022-03-30 00:23:51 +0000

    completed javascript-modal

---
+++ javascript-modal/main.js
@@ -0,0 +1,18 @@
+// get class .modal and assign it to var modal
+// get element button and assign it to var button
+// create function to open modal when button is clicked
+// create NO button within modal to close modal when clicked
+
+var modal = document.querySelector('.modal');
+var button = document.querySelector('button');
+var close = document.querySelector('.close');
+
+button.addEventListener('click', function click() {
+  modal.style.display = 'block';
+}
+);
+
+close.addEventListener('click', function click() {
+  modal.style.display = 'none';
+}
+);
