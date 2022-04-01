b9e170a2b9ed17eb6d4d5c84d451434864cf58cd
Victor-Park <jsxpark@yahoo.com>
2022-04-01 23:55:19 +0000

    javascript-typing-tutor

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,23 +1,15 @@
 var $span = document.querySelectorAll('span');
-
+var index = 0;
 document.addEventListener('keydown', function keydown(event) {
   var $keypressed = event.key;
-  for (var i = 0; i < $span.length; i++) {
-    if ($keypressed === $span[i].textContent) {
-      if ($span[i].className === 'next') {
-        $span[i].className = 'correct';
-        $span[++i].className = 'next';
-      }
-    } else {
-      if ($span[i].className === 'next') {
-        $span[i].className = 'wrong';
-      } else {
-        if ($span[i].className === 'wrong') {
-          if ($keypressed === 'Backspace') {
-            $span[i].className = 'next';
-          }
-        }
-      }
+  if ($keypressed === $span[index].textContent) {
+    $span[index].className = 'correct';
+    $span[++index].className = 'next';
+  } else {
+    $span[index].className = 'wrong';
+    if ($keypressed === 'Backspace') {
+      $span[index].className = 'next';
     }
   }
-});
+})
+;
