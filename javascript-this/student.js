8bd068e45b4d21e4bc4128fce1b2fd38f52f7610
Victor-Park <jsxpark@yahoo.com>
2022-04-11 19:24:12 +0000

    javascript-this

---
+++ javascript-this/student.js
@@ -0,0 +1,14 @@
+/* exported student */
+var student = {
+  firstName: 'Victor',
+  lastName: 'Park',
+  subject: '!Javascript',
+  getFullName: function () {
+    var fullName = this.firstName + ' ' + this.lastName;
+    return fullName;
+  },
+  getIntroduction: function () {
+    var intro = 'Hello, my name is ' + this.firstName + ' ' + this.lastName + ' and I am studying ' + this.subject + '.';
+    return intro;
+  }
+};
