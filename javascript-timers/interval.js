e6f3e38540ea155182a7afbb9168d92525b73cee
Victor-Park <jsxpark@yahoo.com>
2022-04-13 18:21:28 +0000

    third time's the charm

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -4,19 +4,14 @@ function countdown() {
   interval = setInterval(text, 1000);
 }
 
-// if text.textContent = '1'
-// then change it to '~earth below us~'
-// else text.textContent
-
 function text() {
   var text = document.querySelector('h1');
   if (text.textContent !== '1') {
     var count = parseInt(text.textContent);
     --count;
     text.textContent = String(count);
-  } else if (text.textContent === '1') {
-    text.textContent = '~Earth Beeeelooowww Us~';
   } else {
+    text.textContent = '~Earth Beeeelooowww Us~';
     clear();
   }
 }
