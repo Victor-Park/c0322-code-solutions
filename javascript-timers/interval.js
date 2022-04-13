3d26e24bc19dd8ec08ab83ff195da830aa38c4af
Victor-Park <jsxpark@yahoo.com>
2022-04-13 18:10:58 +0000

    revised javascript-timers

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -4,15 +4,19 @@ function countdown() {
   interval = setInterval(text, 1000);
 }
 
+// if text.textContent = '1'
+// then change it to '~earth below us~'
+// else text.textContent
+
 function text() {
   var text = document.querySelector('h1');
-  if (text.textContent === '4') {
-    text.textContent = '3';
-  } else if (text.textContent === '3') {
-    text.textContent = '2';
-  } else if (text.textContent === '2') {
-    text.textContent = '1';
-  } else if (text.textContent === '1') {
+  if (text.textContent !== '1') {
+    var count = parseInt(text.textContent);
+    --count;
+    text.textContent = String(count);
+    return;
+  }
+  if (text.textContent === '1') {
     text.textContent = '~Earth Beeeelooowww Us~';
   } else {
     clear();
